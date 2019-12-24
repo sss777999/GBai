bb6b9318263421e2d4992dd9eb341eb7b1fd2620
sss777999 <spt999@protonmail.com>
2019-12-24 16:43:08 +0300

    Добавлен более оптимальный вариант в Задание 3

--- Основы языка Python/Урок 1 Знакомство с Python/Задание 3.py
+++ Основы языка Python/Урок 1 Знакомство с Python/Задание 3.py
@@ -3,3 +3,9 @@
 
 inp = int(input('Введите число: '))
 print(inp + int((str(inp)+str(inp))) + int((str(inp)+str(inp)+str(inp))))
+
+"""
+Оптимальный вариант
+inp = input('Введите число: ')
+print(int(inp) + int(inp + inp) + int(inp + inp + inp))
+"""
