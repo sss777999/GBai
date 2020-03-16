62b8ca294ad6179e77f7ac9e57de0badb5bd71bd
sss777999 <spt999@protonmail.com>
2020-03-16 15:06:58 +0300

    Lesson8 3 с правильным вариантом

--- Базы данных/Урок 8. Вебинар. Сложные запросы/Lesson8.sql
+++ Базы данных/Урок 8. Вебинар. Сложные запросы/Lesson8.sql
@@ -117,3 +117,19 @@ JOIN messages ON messages.from_user_id = users.id
 GROUP BY 1 
 ORDER BY 2 LIMIT 10;
 
+/* Правильный вариант
+SELECT users.id,
+COUNT(DISTINCT messages.id) +
+COUNT(DISTINCT likes.id) +
+COUNT(DISTINCT media.id) AS activity
+FROM users
+LEFT JOIN messages
+ON users.id = messages.from_user_id
+LEFT JOIN likes
+ON users.id = likes.user_id
+LEFT JOIN media
+ON users.id = media.user_id
+GROUP BY users.id
+ORDER BY activity
+LIMIT 10;
+*/
