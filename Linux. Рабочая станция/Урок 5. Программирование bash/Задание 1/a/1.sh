6acdeddf33624c7e418a75de155b9b0178dfe7ed
sss777999 <spt999@protonmail.com>
2020-05-12 10:54:48 +0300

    Перенос

--- Linux. Рабочая станция/Урок 5. Программирование bash/Задание 1/a/1.sh
+++
@@ -1,3 +0,0 @@
-a=program pwd
-echo $a
-sed 's/[A-Z]/\L/g; /^$/d' 123.txt > output.txt
--- Linux. Рабочая станция/Урок 5. Программирование bash/Задание 1/b/sclog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-trap "echo ' Scrip is DONE. God bye .....'" SIGINT
-echo this is
-echo Script is running ... 
-tail -f /var/log/auth.log | grep failure
