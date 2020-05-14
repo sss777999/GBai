ca355846be71e3b69b1c40108792d473cc553270
sss777999 <spt999@protonmail.com>
2020-05-14 18:57:12 +0300

    Исправление

--- Алгоритмы и структуры данных на Python. Интерактивный курс (10 мая - 24 июня)/Урок 3. Массивы/Задание 4.py
+++ Алгоритмы и структуры данных на Python. Интерактивный курс (10 мая - 24 июня)/Урок 3. Массивы/Задание 4.py
@@ -6,18 +6,15 @@ import random
 a = [random.randint(0, 5) for i in range(random.randint(0, 20))]
 print(a)
 count = 0
-count1 = 0
+count1 = 1
 max = 0
 
 for i in a:
-    count = max
     count1 = 0
     for j in a:
         if i == j:
             count1 += 1
     if count1 > count:
         max = i
-if max == 0:
-    print('Нет повторяющихся чисел')
-else:
-    print(f'Максимальное количество раз в массиве повторяется число: {max}')
+        count = count1
+print(f'Максимальное количество раз в массиве повторяется число: {max}')
